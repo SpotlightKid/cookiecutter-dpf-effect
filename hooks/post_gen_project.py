99aa0bbf89342bfce053dd9855f78001a85ac969
Christopher Arndt <chris@chrisarndt.de>
2018-08-02 15:54:07 +0200

    Improve post-project-generation hook
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Init git repository and add DPF as a git submodule."""
+"""Init git repository and add DPF as a git submodule and make initial commit."""
+
+from __future__ import print_function
 
 from subprocess import call
+
 
 submodule = "dpf"
 submodule_url = "git://github.com/DISTRHO/DPF.git"
 git_url = "git@github.com:{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}"
 
+print("Running post-project-generation hook...")
+
+print("Initializing new Git repository:")
 call(['git', 'init'])
+print("Adding Git submodule for DPF library:")
 call(['git', 'remote', 'add', 'origin', git_url])
+print("Checking out submodules:")
 call(['git', 'submodule', 'add', submodule_url, submodule])
+print("Making initial Git commit:")
+call(['git', 'add', '-A'])
+call(['git', 'commit', '-m', 'Initial commit'])
+
+print("\nYour DPF audio effect plugin project is now ready!")
+print("To compile it, change into the '{{ cookiecutter.repo_name }}' directory and type 'make'.")
+print("The plugin binaries and LV2 bundle will be placed in the 'bin' subdirectory.")
+print("Have fun!")
