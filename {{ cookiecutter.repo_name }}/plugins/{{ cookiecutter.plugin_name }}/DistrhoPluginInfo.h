c82d97cb18cf1a75533bf9d68c9f2ff843df151a
Christopher Arndt <chris@chrisarndt.de>
2024-11-06 22:40:02 +0100

    Add comments to DistrhoInfo.hpp template
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- {{ cookiecutter.repo_name }}/plugins/{{ cookiecutter.plugin_name }}/DistrhoPluginInfo.h
+++ {{ cookiecutter.repo_name }}/plugins/{{ cookiecutter.plugin_name }}/DistrhoPluginInfo.h
@@ -29,9 +29,16 @@
 #ifndef DISTRHO_PLUGIN_INFO_H
 #define DISTRHO_PLUGIN_INFO_H
 
-#define DISTRHO_PLUGIN_BRAND "{{ cookiecutter.plugin_brand }}"
+// The plugin name.
+// This is used to identify your plugin before a Plugin instance can be created.
 #define DISTRHO_PLUGIN_NAME  "{{ cookiecutter.plugin_name }}"
+// The plugin brand name. Used for the LV2 metadata and VST3 UI interface.
+// Must be a valid C++ identifier, i.e. can not contain spaces or dashes.
+#define DISTRHO_PLUGIN_BRAND "{{ cookiecutter.plugin_brand }}"
+// The plugin URI when exporting in LV2 format.
+// See https://lv2plug.in/book/#_manifest_ttl_in
 #define DISTRHO_PLUGIN_URI   "{{ cookiecutter.plugin_uri }}"
+// The plugin id when exporting in CLAP format, in reverse URI form
 #define DISTRHO_PLUGIN_CLAP_ID "{{ cookiecutter.plugin_clap_id }}"
 
 #define DISTRHO_PLUGIN_HAS_UI        {{ 0 if cookiecutter.ui_type == "none" else 1 }}
@@ -45,8 +52,11 @@
 #define DISTRHO_PLUGIN_WANT_MIDI_INPUT  {{ 0 if cookiecutter.want_midi_input == "no" else 1 }}
 #define DISTRHO_PLUGIN_WANT_MIDI_OUTPUT {{ 0 if cookiecutter.want_midi_output == "no" else 1 }}
 
+// See http://lv2plug.in/ns/lv2core#ref-classes
 #define DISTRHO_PLUGIN_LV2_CATEGORY "lv2:AmplifierPlugin"
+// See https://github.com/DISTRHO/DPF/blob/1504e7d327bfe0eac6a889cecd199c963d35532f/distrho/DistrhoInfo.hpp#L717
 #define DISTRHO_PLUGIN_VST3_CATEGORIES "Fx|Tools|Stereo"
+// See https://github.com/DISTRHO/DPF/blob/1504e7d327bfe0eac6a889cecd199c963d35532f/distrho/DistrhoInfo.hpp#L761
 #define DISTRHO_PLUGIN_CLAP_FEATURES "audio-effect", "utility", "stereo"
 
 #endif // DISTRHO_PLUGIN_INFO_H
